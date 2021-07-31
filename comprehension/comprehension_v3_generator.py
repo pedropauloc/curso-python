25048a1660d7bb64fcba44fc2464f15cee4c03ef
Pedro Paulo Carvalho <pedropaulohouston@gmail.com>
2021-07-31 19:57:23 -0300

    Day 14

--- comprehension/comprehension_v3_generator.py
+++ comprehension/comprehension_v3_generator.py
@@ -2,9 +2,9 @@
 # generates data on demand
 
 generator = (i ** 2 for i in range(10) if i % 2 == 0)
-print(next(generator)) # Output 0
-print(next(generator)) # Output 4
-print(next(generator)) # Output 16
-print(next(generator)) # Output 36
-print(next(generator)) # Output 64
+print(next(generator))  # Output 0
+print(next(generator))  # Output 4
+print(next(generator))  # Output 16
+print(next(generator))  # Output 36
+print(next(generator))  # Output 64
 # print(next(generator)) # ERRO
