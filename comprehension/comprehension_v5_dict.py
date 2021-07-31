e5652e8690f83247960bf03948edd017f032a38e
Pedro Paulo Carvalho <50583708+pedropauloc@users.noreply.github.com>
2021-07-31 19:52:49 -0300

    Delete comprehension_v5_dict.py

--- comprehension/comprehension_v5_dict.py
+++
@@ -1,6 +0,0 @@
-# dictionary = {f'Item {i}': i * 2 for i in range(10) if i % 2 == 0}
-dictionary = {i: i * 2 for i in range(10) if i % 2 == 0}
-print(dictionary)
-
-for number, double in dictionary.items():
-    print(f'{number} x 2 = {double}')
