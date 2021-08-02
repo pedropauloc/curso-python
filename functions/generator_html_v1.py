04d428cb489eec280963bd3cb0c72feda21e1615
Pedro Paulo Carvalho <pedropaulohouston@gmail.com>
2021-08-02 16:01:11 -0300

    Day 15

---
+++ functions/generator_html_v1.py
@@ -0,0 +1,11 @@
+def tag_bloco(text, classe='success'):
+    return f'<div class="{classe}">{text}</div>'
+
+if __name__ == '__main__':
+    # Test (assertion) - validates condition, if successful executes normally,
+    # if error returns error message
+    assert tag_bloco('Successfully added!') == \
+        '<div class="success">Successfully added!</div>'
+    assert tag_bloco('Impossible to delete!', 'error') == \
+        '<div class="error">Impossible to delete!</div>'
+    print(tag_bloco('bloco'))
