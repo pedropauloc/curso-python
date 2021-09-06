e6c0d086125475fe813ca5526e87a3d8861b2405
Pedro Paulo Carvalho <pedropaulohouston@gmail.com>
2021-09-06 09:24:29 -0300

    Day 20

---
+++ exercises/sequential/ex_5.py
@@ -0,0 +1,5 @@
+meters = int(input('Enter the amount of meters: '))
+
+centimeters = meters * 100
+
+print(f'Conversion to centimeters: {centimeters}')
---
+++ exercises/sequential/ex_6.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input('Enter the radius: '))
+area = math.pi * (radius ** 2)
+
+print(f'Area: {area}')
